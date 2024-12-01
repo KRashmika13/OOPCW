5ff8e75ad7db88e1f5cc9af4ef097b86a5fcbe01
KRashmika13 <krashmika13@gmail.com>
2024-12-01 16:20:18 +0530

    create new project

---
+++ OOPCW/src/test/java/com/ticketManager/OOPCW/OopcwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketManager.OOPCW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopcwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

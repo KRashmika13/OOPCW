ed56fbc9f86eb808b5539ca20dfef4c7cdbe41ea
KRashmika13 <krashmika13@gmail.com>
2024-12-02 18:49:32 +0530

    did CLI part

--- OOPCW/src/main/java/com/ticketManager/OOPCW/Main.java
+++ OOPCW/src/main/java/com/ticketManager/OOPCW/Main.java
@@ -1,7 +1,21 @@
 package com.ticketManager.OOPCW;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to the Real-Time Event Ticketing System");
+
+        System.out.println("Please enter the total number of tickets(): ");
+        int totalTickets = scan.nextInt();
+
+        System.out.print("Please enter ticket release rate for vendor(): ");
+        int ticketReleaseRate = scan.nextInt();
+
+        System.out.print("Enter maximum ticket capacity ");
+        int maximumTicketCapacity = scan.nextInt();
 
     }
 }

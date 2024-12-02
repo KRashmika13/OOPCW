650f5a5de7a756721f9bc215c9eb171ce28de87f
KRashmika13 <krashmika13@gmail.com>
2024-12-02 19:17:43 +0530

    add instance variables and constructor to this class

--- OOPCW/src/main/java/com/ticketManager/OOPCW/TicketPool.java
+++ OOPCW/src/main/java/com/ticketManager/OOPCW/TicketPool.java
@@ -1,4 +1,17 @@
 package com.ticketManager.OOPCW;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Collection;
+
 public class TicketPool {
+    //instance variables
+    private List<Ticket> tickets;
+    private int maxCapacity;
+
+    //constructor
+    public TicketPool(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+        this.tickets = Collections.synchronizedList(new LinkedList<>());
+    }
 }

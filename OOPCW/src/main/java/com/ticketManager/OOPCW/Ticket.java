3c672a4edc4fe7314ebc2be9a5b367fb8ede2a1d
KRashmika13 <krashmika13@gmail.com>
2024-12-02 19:27:37 +0530

    add instance variables and constructor

--- OOPCW/src/main/java/com/ticketManager/OOPCW/Ticket.java
+++ OOPCW/src/main/java/com/ticketManager/OOPCW/Ticket.java
@@ -1,4 +1,17 @@
 package com.ticketManager.OOPCW;
 
 public class Ticket {
+    //instance variables
+    private int ticketID;
+    private double ticketPrice;
+    private String ticketType;
+
+    //constructor
+    public Ticket(int ticketID, double ticketPrice, String ticketType) {
+        this.ticketID = ticketID;
+        this.ticketPrice = ticketPrice;
+        this.ticketType = ticketType;
+    }
+
+    //
 }

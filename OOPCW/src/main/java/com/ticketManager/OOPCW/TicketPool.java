a6fd394b5792b7c56a8db7a53c0b253c8e9562ad
KRashmika13 <krashmika13@gmail.com>
2024-12-02 21:47:23 +0530

    add addTicket method

--- OOPCW/src/main/java/com/ticketManager/OOPCW/TicketPool.java
+++ OOPCW/src/main/java/com/ticketManager/OOPCW/TicketPool.java
@@ -2,7 +2,7 @@ package com.ticketManager.OOPCW;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Collection;
+import java.util.Collections;
 
 public class TicketPool {
     //instance variables
@@ -13,5 +13,16 @@ public class TicketPool {
     public TicketPool(int maxCapacity) {
         this.maxCapacity = maxCapacity;
         this.tickets = Collections.synchronizedList(new LinkedList<>());
+    }
+
+    public synchronized void addTicket(Ticket ticket) {
+        while (tickets.size() >= maxCapacity) {
+            try{
+                System.out.println("Waiting for space");
+                wait(); //wait until there is space in the pool
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
     }
 }

6ccfebfc9c651c38895f74f11c1eaebff071e6a9
KRashmika13 <krashmika13@gmail.com>
2024-12-03 18:33:24 +0530

    add to instance variables

--- OOPCW/src/main/java/com/ticketManager/OOPCW/Customer.java
+++ OOPCW/src/main/java/com/ticketManager/OOPCW/Customer.java
@@ -1,6 +1,9 @@
 package com.ticketManager.OOPCW;
 
 public class Customer implements Runnable {
+    //instance variables
+    private TicketPool ticketPool;
+    private int customerRetrievalRate; //number of tickets to retrieve customer
 
     @Override
     public void run() {

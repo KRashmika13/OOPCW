8abc279cafe11319ae3ac892b558da9f4d23aee0
KRashmika13 <krashmika13@gmail.com>
2024-12-03 09:07:52 +0530

    add two instance variables

--- OOPCW/src/main/java/com/ticketManager/OOPCW/Vendor.java
+++ OOPCW/src/main/java/com/ticketManager/OOPCW/Vendor.java
@@ -1,6 +1,9 @@
 package com.ticketManager.OOPCW;
 
 public class Vendor implements Runnable{
+    //instance variables
+    private TicketPool ticketPool;
+    private int ticketReleaseRate;
 
     @Override
     public void run() {

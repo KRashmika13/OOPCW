067bf25a365bc9b9be51833e54626ffed4968a4a
KRashmika13 <krashmika13@gmail.com>
2024-12-03 09:12:35 +0530

    add constructor

--- OOPCW/src/main/java/com/ticketManager/OOPCW/Vendor.java
+++ OOPCW/src/main/java/com/ticketManager/OOPCW/Vendor.java
@@ -5,6 +5,11 @@ public class Vendor implements Runnable{
     private TicketPool ticketPool;
     private int ticketReleaseRate;
 
+    //constructor
+    public Vendor(TicketPool ticketPool, int ticketReleaseRate){
+        this.ticketPool = ticketPool;
+        this.ticketReleaseRate = ticketReleaseRate;
+    }
     @Override
     public void run() {
 

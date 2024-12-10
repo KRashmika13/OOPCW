b2c7bf8c1d71d387a2e1e1fbcae93d33f3a28cf7
KRashmika13 <krashmika13@gmail.com>
2024-12-10 15:50:01 +0530

    creat CustomerService

---
+++ OOPCW/src/main/java/com/ticketManager/OOPCW/service/CustomerService.java
@@ -0,0 +1,24 @@
+package com.ticketManager.OOPCW.service;
+
+import com.ticketManager.OOPCW.model.Customer;
+import com.ticketManager.OOPCW.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public Customer saveCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+}

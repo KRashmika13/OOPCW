58be17f5990f546a5c7644c1edeee39bed85c972
KRashmika13 <krashmika13@gmail.com>
2024-12-10 12:03:16 +0530

    add Table annotation

--- OOPCW/src/main/java/com/ticketManager/OOPCW/model/Ticket.java
+++ OOPCW/src/main/java/com/ticketManager/OOPCW/model/Ticket.java
@@ -3,8 +3,10 @@ package com.ticketManager.OOPCW.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "tickets")
 public class Ticket {
 
     //instance variables

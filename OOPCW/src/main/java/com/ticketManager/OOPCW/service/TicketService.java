ca960a5a662a26a6636af750b08cdfa383c78855
KRashmika13 <krashmika13@gmail.com>
2024-12-10 20:55:18 +0530

    create TicketService interface

--- OOPCW/src/main/java/com/ticketManager/OOPCW/service/TicketService.java
+++ OOPCW/src/main/java/com/ticketManager/OOPCW/service/TicketService.java
@@ -1,24 +1,12 @@
 package com.ticketManager.OOPCW.service;
 
+import com.ticketManager.OOPCW.dto.TicketDTO;
 import com.ticketManager.OOPCW.model.Ticket;
-import com.ticketManager.OOPCW.repository.TicketRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class TicketService {
-    private final TicketRepository ticketRepository;
-
-    public TicketService(TicketRepository ticketRepository) {
-        this.ticketRepository = ticketRepository;
-    }
-
-    public List<Ticket> getAllTickets() {
-        return ticketRepository.findAll();
-    }
-
-    public Ticket saveTicket(Ticket ticket) {
-        return ticketRepository.save(ticket);
-    }
+public interface TicketService {
+    Ticket saveTicket(TicketDTO ticketDTO);
+    List<Ticket> getTicketsByEvent(String event);
+    Ticket getTicketByTicketID(String ticketID);
 }

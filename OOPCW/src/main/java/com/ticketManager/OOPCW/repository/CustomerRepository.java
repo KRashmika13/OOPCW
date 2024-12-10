2c2cc9a3176d08c3ad59a6bd3241322de5eb262a
KRashmika13 <krashmika13@gmail.com>
2024-12-10 12:02:43 +0530

    creat CustomerRepository

--- OOPCW/src/main/java/com/ticketManager/OOPCW/repository/CustomerRepository.java
+++ OOPCW/src/main/java/com/ticketManager/OOPCW/repository/CustomerRepository.java
@@ -1,10 +1,11 @@
 package com.ticketManager.OOPCW.repository;
 
+import com.ticketManager.OOPCW.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<CustomerRepository, String> {
+public interface CustomerRepository extends JpaRepository<Customer, String> {
 
     //custom query to find customer by email
     CustomerRepository findByEmail(String email);
